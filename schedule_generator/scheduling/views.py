92e0f649c34ed872089fd6836f43a0135a52ccd1
BenBayotas <elijahben.bayotas@gmail.com>
2024-09-30 13:25:48 +0800

    added schedule view function

--- schedule_generator/scheduling/views.py
+++ schedule_generator/scheduling/views.py
@@ -1,21 +1,16 @@
 from django.shortcuts import render
-from django.shortcuts import render
-from .models import Department, Course, Section, Subject, Instructor, Room, TimeSlot
-from .genetic_algo import GeneticAlgorithm
+from django.http import JsonResponse
+from .genetic_algorithm import GeneticAlgorithm
 
-def generate_schedule(request):
-    departments = Department.objects.all()
-    courses = Course.objects.all()
-    sections = Section.objects.all()
-    subjects = Subject.objects.all()
-    instructors = Instructor.objects.all()
-    rooms = Room.objects.all()
-    timeslots = TimeSlot.objects.all()
 
-    # Instantiate and run the genetic algorithm
-    ga = GeneticAlgorithm(population_size=20, generations=100, mutation_rate=0.05)
-    initial_population = ga.initialize_population(departments, courses, sections, subjects, instructors, rooms, timeslots)
-    best_schedule = ga.run(initial_population, instructors, rooms, timeslots)
-
-    return render(request, 'schedule.html', {'schedule': best_schedule})
 # Create your views here.
+
+def schedule_view(request):
+    
+   if request.method == 'POST'
+
+        ga = GeneticAlgorithm(population_size=50, generations=100, mutation_rate=0.01)
+        best_schedule = ga.evolve()
+
+        return JsonResponse({'schedule': best_schedule})
+    return render(request, 'schedule.html')
